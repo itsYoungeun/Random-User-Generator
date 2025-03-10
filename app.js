24879c108e7b1f845bc240676c390a12a92838a6
Peter <youngeunpc@yahoo.com>
2025-03-10 08:38:32 -0300

    Hardcoded get random user

--- app.js
+++ app.js
@@ -8,7 +8,16 @@ const app = Vue.createApp({
             gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/10.jpg',
         }
-    }
+    },
+    methods: {
+        getUser() {
+            this.firstName = 'Sam',
+            this.lastName = 'Smith',
+            this.email = 'sam@gmail.com',
+            this.gender = 'female',
+            this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
+        },
+    },
 })
 
 app.mount('#app')
